66e038fa4fb14643d514b79cbe48d2126ce96164
RexshackGaming <rexshackuk@gmail.com>
2024-05-16 16:38:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# rsg-animations
+
+- converted to run on RSG full credit goes to original creator XakraD
+https://github.com/XakraD/xakra_animations
---
+++ fxmanifest.lua
@@ -0,0 +1,35 @@
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+
+description 'rsg-example'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@rsg-core/shared/locale.lua',
+    'locales/en.lua', -- preferred language
+    'config.lua',
+}
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua',
+    '@oxmysql/lib/MySQL.lua'
+}
+
+dependencies {
+    'rsg-core',
+    'ox_lib',
+}
+
+files {
+    'ui/**/*',
+}
+
+ui_page 'ui/index.html'
+
+lua54 'yes'
