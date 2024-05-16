efae9018c1c38528677c9aa3f2776c1c79dd0bcf
RexshackGaming <rexshackuk@gmail.com>
2024-05-16 16:42:40 +0100

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # rsg-animations
 
 - converted to run on RSG full credit goes to original creator XakraD
-https://github.com/XakraD/xakra_animations
+- https://github.com/XakraD/xakra_animations
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
 
-description 'rsg-example'
+description 'rsg-animations'
 version '1.0.0'
 
 shared_scripts {
